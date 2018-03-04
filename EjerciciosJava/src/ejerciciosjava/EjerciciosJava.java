c6b93aed7126379450f33cd19df89918f643581d
Fernando <Fernando@King-Ragnar>
2018-03-04 20:35:53 +0100

    Prueba Git

---
+++ EjerciciosJava/src/ejerciciosjava/EjerciciosJava.java
@@ -0,0 +1,14 @@
+
+package ejerciciosjava;
+
+public class EjerciciosJava {
+
+    
+    public static void main(String[] args) {
+        
+        int num1=1, num2=7;
+        
+        System.out.println(num1+ " + "+num1+" = "+num2);
+    }
+    
+}
