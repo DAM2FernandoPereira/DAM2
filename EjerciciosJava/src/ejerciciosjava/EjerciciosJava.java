a913fd75a6699a6e95f7d0f26f4b079e5992fdc6
Fernando <Fernando@King-Ragnar>
2018-03-04 20:49:15 +0100

    Prueba Git2

--- EjerciciosJava/src/ejerciciosjava/EjerciciosJava.java
+++ EjerciciosJava/src/ejerciciosjava/EjerciciosJava.java
@@ -6,9 +6,15 @@ public class EjerciciosJava {
     
     public static void main(String[] args) {
         
-        int num1=1, num2=7;
+        int num1=1, num2=7, num3=0;
         
         System.out.println(num1+ " + "+num1+" = "+num2);
+        System.out.println("¿Quien me lo iba a decir?");
+        
+        
+        
+        
+        System.out.println(num3);
     }
     
 }
